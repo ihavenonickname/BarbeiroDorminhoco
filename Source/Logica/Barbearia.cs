9aaa880116b800780249055c544c182e73f7fba8
ihavenonickname <gbsm1993@gmail.com>
2016-03-04 00:52:02 -0300

    commitando primeiro esboço
    a estrutura das classes será mais ou menos essa, trabalhando com troca
    de mensagens através de eventos

---
+++ Source/Logica/Barbearia.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Barbeiro_Dorminhoco.Logica
+{
+    class Barbearia
+    {
+        private const int QUANTIDADE_MAXIMA_CLIENTES = 5;
+
+        public delegate void IniciarAtendimentoHandler(Cliente cliente);
+
+        public event IniciarAtendimentoHandler IniciarAtendimento;
+
+        private Queue<Cliente> filaDeEspera = new Queue<Cliente>();
+        private Timer timerFilaDeEspera;
+        private Barbeiro barbeiro;
+
+        public void iniciaAtendimentos()
+        {
+            TimerCallback tcb = new TimerCallback((o) =>
+            {
+                int qtde = new Random().Next(QUANTIDADE_MAXIMA_CLIENTES);
+
+                if (qtde > 0)
+                {
+                    for (var i = 0; i < qtde || filaDeEspera.Count < QUANTIDADE_MAXIMA_CLIENTES; i++)
+                        filaDeEspera.Enqueue(new Cliente());
+
+
+                }
+                
+            });
+
+            timerFilaDeEspera = new Timer(tcb, null, 100, 10000);
+        }
+
+        public void interrompeAtendimentos()
+        {
+            timerFilaDeEspera.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+    }
+}
---
+++ Source/Logica/Barbeiro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Barbeiro_Dorminhoco.Logica
+{
+    public class Barbeiro
+    {
+        public delegate void FinaluzouCorteHandler();
+
+        public event FinaluzouCorteHandler FinaluzouCorte;
+
+        private Barbearia barbearia;
+        private Timer timerTesourada;
+
+        private void IniciarCorte(Cliente cliente)
+        {
+            TimerCallback tcb = new TimerCallback((o) =>
+            {
+                cliente.QuantidadeCabelo -= 1;
+
+                cliente.CortouTudo += () =>
+                {
+                    timerTesourada.Change(Timeout.Infinite, Timeout.Infinite);
+                    FinaluzouCorte();
+                };
+            });
+
+            timerTesourada = new Timer(tcb, null, 10, 10);
+        }
+    }
+}
